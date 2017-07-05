5fac077f036ab9c4c847ddf31830c8b4c94ddc1d
Robert Hutchins <rjhutchins22@gmail.com>
2017-07-05 12:06:17 -0400

    add files to git repo for starting

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const mustacheExpress = require('mustache-express');
+mongoose.Promise = require('bluebird');
+var app = express();
+const bodyParser = require('body-parser');
+var app = express();
+// Replace "test" with your database name.
+const User = require('./user.js');
+mongoose.connect('mongodb://localhost:27017/test');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+// app.use(express.static('./public'));
+
+
+app.get('/', function(req, res){
+  User.find().then(function(user){
+
+  res.render('index', {users: user});
+}).catch(function(){
+  console.log('sorry something when wrong.')
+})
+})
+
+app.listen(3000, function(req, res){
+  console.log('You are here.');
+})
+
+
+
+
+
+
+
+// var user = new User({firstName: "Jarule", lastName: "Canada"});
+
+// User.updateOne({firstName:'Waffle'},{
+//
+//   firstName: 'WafflePan',
+//   $push : {addresses: {
+//     street: "Richmond",
+//     city: "Housten"
+//   }}
+
+// }).catch(function(error, affected, resp){
+//   console.log(error);
+// })
+
+
+// user.save().then(function(){
+//   console.log("This user has been saved.");
+// })
+// .catch(function(){
+//   console.log('something has happened');
+// })
+// console.log(user);
+//
+//
+// User.create({firstName: 'Jim', lastName: "Bean"}).then(function(){
+//   console.log("He has been saved too.");
+// }).catch(function(){
+//   console.log('Yo something happened down here.');
+// })
+//
+// console.log(user);
